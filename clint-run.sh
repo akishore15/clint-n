80178a89e52f29765a42d20f8ef49015b0241aeb
hitor- 1 <154870205+akishore15@users.noreply.github.com>
2024-12-07 22:08:22 -0800

    clint-run.sh

--- clint-run.sh
+++ clint-run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt install git
+sudo ufw allow 85/tcp
 git clone https://github.com/akishore15/clint-n.git --p 85
 echo "GIT CLONED, MAPPED AT localhost:85"
 echo "CONTINUE [Y / N]"
